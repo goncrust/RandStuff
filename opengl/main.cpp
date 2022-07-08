52641bc4369a0acb3ddee2ed8af60bd2913e1870
goncrust <goncalonrua@protonmail.com>
2022-07-08 16:39:34 +0100

    vertex buffers and drawing a triangle

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void)
-{
-    GLFWwindow* window;
+int main(void) {
+    GLFWwindow *window;
 
     /* Initialize glfw */
     if (!glfwInit())
@@ -12,10 +11,9 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1080, 760, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
+    if (!window) {
+    glfwTerminate();
+    return -1;
     }
 
     /* Make the window's context current */
@@ -25,13 +23,24 @@ int main(void)
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
+    /* Print version */
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    /* -------------- Setup Vertex Buffers ----------------- */
+    float positions[] = { -0.5, -0.5, 0.0, 0.5, 0.5, -0.5 };
+
+    unsigned int buffer_id;
+    glGenBuffers(1, &buffer_id); // Generate one buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_id); // Select the buffer
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW); // Write data to buffer
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /* ------------------- Draw ------------------------ */
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Draw vertices (from 0 to third)
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
